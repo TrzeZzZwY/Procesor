5613e6dab691085c29ba0da95b6440a06b97077c
Jan Świder <jasiek.swider@outlook.com>
2022-03-18 09:49:40 +0100

    Dodaj pliki projektów.

---
+++ Procesor/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Procesor
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Procesor/Rejestr.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Procesor
+{
+    public class Rejestr
+    {
+        private string _name;
+        public string Name { get => _name; set => _name = (value.Length == 2 && Regex.IsMatch(value, @"^[a-zA-Z]+$")) ? value.ToUpper() : throw new ArgumentException("Nazwa rejestru powinna być 2 literowa"); }
+
+        private byte _value;
+        public string Value { get => _value.ToString("x2").ToUpper(); set => _value = Byte.Parse(value); }
+        public Rejestr(string name, int value = 0)
+        {
+            Name = name;
+            Value = value.ToString();
+        }
+        public override string ToString() => $"Name: {Name}\tValue: {Value} ";
+        public static Rejestr operator +(Rejestr a, int b) => new Rejestr(a.Name, (byte)((Convert.ToByte(a.Value, 16) + b)));
+        public static Rejestr operator -(Rejestr a, int b) => new Rejestr(a.Name, (byte)((Convert.ToByte(a.Value, 16) - b)));
+        public static Rejestr operator ~(Rejestr a) => new Rejestr(a.Name, (byte)~((Convert.ToByte(a.Value, 16) )));
+
+        public static void MOV(Rejestr a, Rejestr b) => a.Value = b.Value;
+        public static void XCHG(Rejestr a, Rejestr b)
+        {
+            var temp = a.Value;
+            a.Value = b.Value;
+            b.Value = temp;
+        }
+        public static void INC(ref Rejestr a) => a += 1;
+        public static void DEC(ref Rejestr a) => a -= 1;
+        public static void NOT(ref Rejestr a) => a = ~a;
+        public static void NEG(ref Rejestr a) => a = (~a)+1;
+    }
+}
